6e233f0a7c9123f20d48db16ff9178654ef31b69
ASyrotenko <79544620+ASyrotenko@users.noreply.github.com>
2022-09-13 20:05:02 +0000

    Deploying to gh-pages from @ ASyrotenko/command-project-html-css@7076430943f9e1a9f2a2aca0df0692ee39f36b9d 🚀

---
+++ contacts.0f0b950b.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".js-lang-menu-container"),t=document.querySelector(".js-open-lang-menu");t.addEventListener("click",(function(){var n="true"===t.getAttribute("aria-expanded")||!1;t.setAttribute("aria-expanded",!n),e.classList.toggle("is-open"),bodyScrollLock[n?"enableBodyScroll":"disableBodyScroll"](document.body)}))}();
+//# sourceMappingURL=contacts.0f0b950b.js.map
